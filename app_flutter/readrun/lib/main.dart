04fecaeadd39fd7f4e3b5c7a5043ee852adbf55c
PushkaraSharma <pushkarasharma11@gmail.com>
2020-07-03 16:45:58 +0530

    Flutter prototype ui development1

--- app_flutter/readrun/lib/main.dart
+++ app_flutter/readrun/lib/main.dart
@@ -1,18 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:readrun/testing.dart';
-void main() => runApp(new MyApp());
+import 'dart:async';
+
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'ZeroKata',
-      theme: new ThemeData(
 
-        primarySwatch: Colors.grey,
-      ),
-      home:  MainFetchData(),
+    return MaterialApp(
+        home: Scaffold(body: MainFetchData())
     );
   }
 }
--- app_flutter/readrun/lib/testing.dart
+++ app_flutter/readrun/lib/testing.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
 import 'package:http/http.dart' as http;
 import 'package:readrun/News.dart';
 
@@ -9,15 +10,20 @@ class MainFetchData extends StatefulWidget {
 }
 
 class _MainFetchDataState extends State<MainFetchData> {
+  final PageController ctrl = PageController(viewportFraction: 0.95);
+  String activeTag = 'top_stories';
+  int currentPage = 0;
+
   List<News> list = List();
   var isLoading = false;
 
-  _fetchData() async {
+  _fetchData({ String tag ='top_stories' }) async {
     setState(() {
       isLoading = true;
+      activeTag = tag;
     });
-    final response =
-    await http.get("http://35.226.196.3:5000/top_stories/");
+
+    final response = await http.get("http://34.72.182.102:5000/"+activeTag+"/");
     if (response.statusCode == 200) {
       list = (json.decode(response.body) as List).map((data) => new News.fromJson(data)).toList();
       print(list[0].heading);
@@ -30,35 +36,92 @@ class _MainFetchDataState extends State<MainFetchData> {
   }
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: Text("Fetch Data JSON"),
-        ),
-        bottomNavigationBar: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: RaisedButton(
-            child: new Text("Fetch Data"),
-            onPressed: _fetchData,
-          ),
-        ),
-        body: isLoading
-            ? Center(
-          child: CircularProgressIndicator(),
-        )
-            : ListView.builder(
-            itemCount: list.length,
-            itemBuilder: (BuildContext context, int index) {
-              return ListTile(
-                contentPadding: EdgeInsets.all(10.0),
-                title: new Text(list[index].heading),
-                trailing: new Image.network(
-                  list[index].picUrl,
-                  fit: BoxFit.cover,
-                  height: 40.0,
-                  width: 40.0,
-                ),
-              );
-            }));
+  void initState() {
+    _fetchData(tag:activeTag);
+    // Set state when page changes
+    ctrl.addListener(() {
+      int next = ctrl.page.round();
+
+      if(currentPage != next) {
+        setState(() {
+          currentPage = next;
+        });
+      }
+    });
   }
+
+  @override
+  Widget build(BuildContext context) {
+
+    return PageView.builder(
+
+        controller: ctrl,
+        itemCount: list.length + 1,
+        itemBuilder: (context, int currentIdx) {
+          if (currentIdx == 0) {
+            return _buildTagPage();
+          } else if (list.length >= currentIdx) {
+            // Active page
+            bool active = currentIdx == currentPage;
+            return _buildStoryPage(list[currentIdx - 1], active);
+          }
+        }
+    );
+  }
+
+  // Builder Functions
+  _buildStoryPage(News data, bool active) {
+    // Animated Properties
+    final double blur = active ? 30 : 0;
+    final double offset = active ? 20 : 0;
+    final double top = active ? 100 : 200;
+
+
+    return AnimatedContainer(
+        duration: Duration(milliseconds: 500),
+        curve: Curves.easeOutQuint,
+        margin: EdgeInsets.only(top: top, bottom: 50, right: 30),
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(20),
+
+            image: DecorationImage(
+              fit: BoxFit.cover,
+              image: NetworkImage(data.picUrl),
+            ),
+
+            boxShadow: [BoxShadow(color: Colors.black87, blurRadius: blur, offset: Offset(offset, offset))]
+        ),
+        child: Align(alignment: Alignment.bottomCenter,
+            child:Container(decoration: BoxDecoration(borderRadius: BorderRadius.circular(10),color: Colors.white),
+                padding: EdgeInsets.all(10),
+    child:Text(data.heading, style: TextStyle(fontSize: 25, color: Colors.black)))
+    ));
+  }
+
+
+  _buildTagPage() {
+    return Container(child:
+    Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.start,
+
+      children: [
+        Text('READ RUN', style: TextStyle(fontSize: 50, fontWeight: FontWeight.bold,color: Colors.black),),
+        Text('Your Stories', style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),),
+        Text('FILTER', style: TextStyle( color: Colors.black26 )),
+        _buildButton('top_stories'),
+        _buildButton('gaming'),
+        _buildButton('tech'),
+        _buildButton('sports')
+      ],
+    )
+    );
+  }
+
+  _buildButton(tag) {
+    Color color = tag == activeTag ? Colors.purple : Colors.white;
+    return FlatButton(color: color, child: Text('#$tag'), onPressed: () => _fetchData(tag: tag));
+  }
+
 }
+
