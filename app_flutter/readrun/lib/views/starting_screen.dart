33310aacd1c86b988aa675ce892c22643939a2b5
PushkaraSharma <pushkarasharma11@gmail.com>
2020-07-14 18:42:26 +0530

    sidebar with ui changes

--- app_flutter/readrun/lib/views/starting_screen.dart
+++ app_flutter/readrun/lib/views/starting_screen.dart
@@ -5,13 +5,15 @@ import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/painting.dart';
 import 'package:flutter/widgets.dart';
+import 'package:readrun/Widgets/waveclip.dart';
 import 'package:readrun/views/no_internet.dart';
+import 'package:swipedetector/swipedetector.dart';
 import 'package:wave/config.dart';
 import 'package:wave/wave.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:readrun/views/information.dart';
 import 'package:google_fonts/google_fonts.dart';
-
+import 'package:foldable_sidebar/foldable_sidebar.dart';
 
 class StartScreen extends StatefulWidget {
   @override
@@ -19,7 +21,60 @@ class StartScreen extends StatefulWidget {
 }
 
 class _StartScreenState extends State<StartScreen> {
+  FSBStatus drawerStatus;
 
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: SwipeDetector(
+          onSwipeRight: () {
+            setState(() {
+              drawerStatus = FSBStatus.FSB_OPEN;
+            });
+          },
+          onSwipeLeft: () {
+            setState(() {
+              drawerStatus = FSBStatus.FSB_CLOSE;
+            });
+          },
+          child: FoldableSidebarBuilder(
+            drawerBackgroundColor: Color(0xffFFBD95),
+            drawer: CustomDrawer(
+              closeDrawer: () {
+                setState(() {
+                  drawerStatus = FSBStatus.FSB_CLOSE;
+                });
+              },
+            ),
+            screenContents: HomePage(),
+            status: drawerStatus,
+          ),
+        ),
+//        floatingActionButton: FloatingActionButton(
+//          mini: true,
+//            backgroundColor: Color(0xffFFBD95),
+//            child: floatButtonType?Icon(Icons.menu, color: Colors.white,):Icon(Icons.close, color: Colors.white,),
+//            onPressed: () {
+//              setState(() {
+//                floatButtonType = true;
+//                drawerStatus = drawerStatus == FSBStatus.FSB_OPEN
+//                    ? FSBStatus.FSB_CLOSE
+//                    : FSBStatus.FSB_OPEN;
+//              });
+//
+//            }),
+      ),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   _buildCard({Config config, Color backgroundColor = Colors.transparent}) {
     return Container(
       color: Colors.transparent,
@@ -42,16 +97,16 @@ class _StartScreenState extends State<StartScreen> {
     MaskFilter.blur(BlurStyle.outer, 10.0),
     MaskFilter.blur(BlurStyle.solid, 16.0),
   ];
-  int _blurIndex = 3;
+  int _blurIndex = 0;
 
   MaskFilter _nextBlur() {
     if (_blurIndex == _blurs.length - 1) {
-      _blurIndex = 0;
+      _blurIndex = 1;
     } else {
       _blurIndex = _blurIndex + 1;
     }
-    _blur = _blurs[_blurIndex];
-    return _blurs[_blurIndex];
+    _blur = _blurs[4];
+    return _blurs[4];
   }
 
   @override
@@ -65,7 +120,7 @@ class _StartScreenState extends State<StartScreen> {
         children: <Widget>[
           Stack(children: <Widget>[
             Container(
-              padding: EdgeInsets.fromLTRB(0, 10, 0, 0),
+              padding: EdgeInsets.fromLTRB(0, 0, 0, 0),
               //height: height*0.3,width:width,
               color: Color(0xffFFBD95),
               child: Column(
@@ -140,17 +195,17 @@ class _StartScreenState extends State<StartScreen> {
                 ],
               ),
             ),
-            Positioned(
-                top: 20,
-                left: 0.0,
-
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Icon(Icons.settings,
-                      color:  Colors.black,
-                      size: 27.0),
-                )
-            ),
+//            Positioned(
+//                top: 20,
+//                left: 0.0,
+//
+//                child: Padding(
+//                  padding: const EdgeInsets.all(8.0),
+//                  child: Icon(Icons.settings,
+//                      color:  Colors.black,
+//                      size: 27.0),
+//                )
+//            ),
           ]),
           Container(
               padding: EdgeInsets.all(15),
@@ -170,42 +225,39 @@ class _StartScreenState extends State<StartScreen> {
               mainAxisSpacing: 15.0,
               padding: EdgeInsets.symmetric(horizontal: 8.0, vertical: 5.0),
               children: <Widget>[
-                myitems(
-                    'Top Stories',
-                    'assets/images/undraw_newspaper_k72w.png',
-                    "top_stories"),
-                myitems(
-                    'Sports',
-                    'assets/images/undraw_track_and_field_33qn.png',
-                    "sports"),
+                myitems('Top Stories',
+                    'assets/images/undraw_newspaper_k72w.png', "top_stories"),
+                myitems('Sports',
+                    'assets/images/undraw_track_and_field_33qn.png', "sports"),
                 myitems(
                     'Technology',
                     'assets/images/undraw_visionary_technology_33jy.png',
                     "tech"),
                 myitems(
-                    'Gaming',
-                    'assets/images/undraw_gaming_6oy3.png',
-                    "gaming",),
+                  'Gaming',
+                  'assets/images/undraw_gaming_6oy3.png',
+                  "gaming",
+                ),
                 myitems(
-                    'Fashion',
-                    'assets/images/fashion_pic.png',
-                    "gaming",
-                    ),
+                  'Fashion',
+                  'assets/images/fashion_pic.png',
+                  "gaming",
+                ),
                 myitems(
-                    'Automobile',
-                    'assets/images/undraw_city_driver_jh2h.png',
-                    "gaming",
-                    ),
+                  'Automobile',
+                  'assets/images/undraw_city_driver_jh2h.png',
+                  "gaming",
+                ),
                 myitems(
-                    'International',
-                    'assets/images/undraw_around_the_world_v9nu.png',
-                    "gaming",
-                    ),
+                  'International',
+                  'assets/images/undraw_around_the_world_v9nu.png',
+                  "gaming",
+                ),
                 myitems(
-                    'Lifestyle',
-                    'assets/images/undraw_healthy_habit_bh5w.png',
-                    "gaming",
-                    ),
+                  'Lifestyle',
+                  'assets/images/undraw_healthy_habit_bh5w.png',
+                  "gaming",
+                ),
               ],
               staggeredTiles: [
                 StaggeredTile.extent(1, 160),
@@ -232,28 +284,39 @@ class _StartScreenState extends State<StartScreen> {
         borderRadius: BorderRadius.circular(20.0),
         child: InkResponse(
           onTap: () {
-              try {
-                InternetAddress.lookup('google.com').then((result) {
-                  if (result.isNotEmpty &&
-                      result[0].rawAddress.isNotEmpty) {
-                    Navigator.pushAndRemoveUntil(context,
-                        MaterialPageRoute(builder: (BuildContext context) => Information(topic:topic)),
-                            (Route<dynamic> route) => route is StartScreen);
-                  } else {
-                    print('No internet');
-                    Navigator.push(context, MaterialPageRoute(builder: (context) => No_internet(topic:topic)),
-                    );
-                  }
-                }).catchError((error) {
+            try {
+              InternetAddress.lookup('google.com').then((result) {
+                if (result.isNotEmpty && result[0].rawAddress.isNotEmpty) {
+                  Navigator.pushAndRemoveUntil(
+                      context,
+                      MaterialPageRoute(
+                          builder: (BuildContext context) =>
+                              Information(topic: topic)),
+                      (Route<dynamic> route) => route is StartScreen);
+                } else {
                   print('No internet');
-                  Navigator.push(context, MaterialPageRoute(builder: (context) => No_internet(topic:topic)),);
-                });
-              } on SocketException catch (_) {
-                Navigator.push(context, MaterialPageRoute(builder: (context) => No_internet(topic:topic)),);
-              }
-
-
-      },
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => No_internet(topic: topic)),
+                  );
+                }
+              }).catchError((error) {
+                print('No internet');
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => No_internet(topic: topic)),
+                );
+              });
+            } on SocketException catch (_) {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => No_internet(topic: topic)),
+              );
+            }
+          },
           radius: 10.0,
           containedInkWell: false,
           child: Center(
@@ -282,5 +345,97 @@ class _StartScreenState extends State<StartScreen> {
             ),
           ),
         ));
+  }
+}
+
+class CustomDrawer extends StatelessWidget {
+  final Function closeDrawer;
+
+  const CustomDrawer({Key key, this.closeDrawer}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    MediaQueryData mediaQuery = MediaQuery.of(context);
+
+    return Container(
+      //color: Color(0xffFFDDC8),
+      color:Colors.white,
+      width: mediaQuery.size.width * 0.60,
+      height: mediaQuery.size.height,
+      child: Column(
+        children: <Widget>[
+            Container(
+              color: Color(0xffeee7eb),
+              height: mediaQuery.size.height*0.22,
+              width: mediaQuery.size.width * 0.60,
+              child: Image(
+                image: AssetImage('assets/images/4.gif'),
+                fit: BoxFit.fill,
+              ),
+            ),
+            ClipPath(
+              clipper: WaveClipper(),
+              child: Container(
+                height: 100,
+                color: Color(0xffeee7eb),
+              ),
+            ),
+
+          ListTile(
+            onTap: () {
+              debugPrint("Tapped Profile");
+            },
+            leading: Icon(Icons.person),
+            title: Text(
+              "Your Profile",
+            ),
+          ),
+          Divider(
+            height: 1,
+            color: Colors.grey,
+          ),
+          ListTile(
+            onTap: () {
+              debugPrint("Tapped settings");
+            },
+            leading: Icon(Icons.settings),
+            title: Text("Settings"),
+          ),
+          Divider(
+            height: 1,
+            color: Colors.grey,
+          ),
+          ListTile(
+            onTap: () {
+              debugPrint("Tapped Payments");
+            },
+            leading: Icon(Icons.payment),
+            title: Text("Payments"),
+          ),
+          Divider(
+            height: 1,
+            color: Colors.grey,
+          ),
+          ListTile(
+            onTap: () {
+              debugPrint("Tapped Notifications");
+            },
+            leading: Icon(Icons.notifications),
+            title: Text("Notifications"),
+          ),
+          Divider(
+            height: 1,
+            color: Colors.grey,
+          ),
+          ListTile(
+            onTap: () {
+              debugPrint("Tapped Log Out");
+            },
+            leading: Icon(Icons.exit_to_app),
+            title: Text("Log Out"),
+          ),
+        ],
+      ),
+    );
   }
 }
